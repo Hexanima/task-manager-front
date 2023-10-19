f2d5837c695cc0ed027058e9c03f1bff412b4f4d
Hexanima <nicolasfilippelli22@gmail.com>
2023-10-19 20:12:29 -0300

    Little adjustments added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,16 @@ function Home() {
     <>
       <TaskInput />
       {(matches && (
-        <div style={{ display: "flex", gap: "1rem", width:"100%" }}>
+        <div style={{ display: "flex", gap: "1rem", width: "100%" }}>
           <TaskList filter="Pendiente" />
           <TaskList filter="En progreso" />
           <TaskList filter="Terminada" />
         </div>
-      )) || <TaskList />}
+      )) || (
+        <div style={{ display: "flex", gap: "1rem", width: "100%" }}>
+          <TaskList />
+        </div>
+      )}
     </>
   );
 }
